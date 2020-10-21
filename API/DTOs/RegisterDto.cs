b754a05bc94620734dde9a7bc3f125b7a8d81abf
Clint Brodar <clintbrodar@gmail.com>
2020-10-21 14:58:25 -0700

    validate user inputs

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ASPNETAngularDemo.API.DTOs
 {
     public class RegisterDto
     {
+        [Required]
         public string UserName { get; set; }
+        [Required]
+        [StringLength(25, ErrorMessage = "Password cannot exceed 25 characters")]
         public string Password { get; set; }
     }
 }
