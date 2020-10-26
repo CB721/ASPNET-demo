a5f0313a3355b3797d260d4e1e19657f90ce6478
Clint Brodar <clintbrodar@gmail.com>
2020-10-26 11:03:08 -0700

    login method

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
   title = 'Dating App';
   model: any = {};
+  loggedIn: Boolean;
 
-  constructor() { }
+  constructor(private accountService: AccountService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   login() {
-    console.log(this.model)
+    this.accountService.login(this.model).subscribe(
+      (res) => {
+        console.log(res);
+        this.loggedIn = true;
+      },
+      (err) => console.log(err)
+    );
   }
 }
