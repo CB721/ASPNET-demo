b2b52288befd982296ea0b225d755a68b6aefc89
Clint Brodar <clintbrodar@gmail.com>
2020-10-26 12:34:02 -0700

    home page component

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  isRegisterMode: Boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+  registerToggle() {
+    this.isRegisterMode = !this.isRegisterMode;
+  }
+}
