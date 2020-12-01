36ad7d9a8da191f723947c9dc0b01b74d06d84fa
Clint Brodar <clintbrodar@gmail.com>
2020-12-01 14:20:50 -0800

    register form validation

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -47,6 +47,7 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     ShareModule,
     NgxSpinnerModule
   ],
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -9,23 +16,61 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-
   model: any = {};
+  registerForm: FormGroup;
+
   constructor(
     private accountService: AccountService,
     private toastr: ToastrService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(25),
+        this.isValidPass(),
+      ]),
+      confirmPassword: new FormControl('', [
+        Validators.required,
+        this.matchPass('password'),
+      ]),
+    });
+  }
+
+  matchPass(matchTo: any): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control?.value === control?.parent?.controls[matchTo].value
+        ? null
+        : { isMatching: true };
+    };
+  }
+
+  isValidPass(): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control?.value?.match(
+        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/
+      )?.length
+        ? null
+        : { isValid: true };
+    };
+  }
 
   register() {
-    this.accountService.register(this.model).subscribe(
-      () => this.cancel(),
-      (err) => {
-        console.log(err);
-        this.toastr.error(err.error);
-      }
-    );
+    console.log(this.registerForm.value);
+    // this.accountService.register(this.model).subscribe(
+    //   () => this.cancel(),
+    //   (err) => {
+    //     console.log(err);
+    //     this.toastr.error(err.error);
+    //   }
+    // );
   }
   cancel() {
     this.cancelRegister.emit(false);
